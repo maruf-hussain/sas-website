7efc7f77ba37745596ae9fa9b58e4e49d9bcf70b
maruf-hussain <engmarufhussain@gmaile.com>
2022-06-06 16:05:58 +0600

    we do added

---
+++ src/Component/Banner/Banner.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+const Banner = () => {
+    return (
+        <div class='mt-5'>
+        <Carousel>
+          <Carousel.Item interval={1000}>
+            <img
+              className="d-block w-100 mx-auto"
+              src="https://t4.ftcdn.net/jpg/02/44/58/27/240_F_244582708_ZJGzPhHSK36iIlE1IosIRQe7553D4ThU.jpg"
+              alt="First slide"
+            />
+            
+          </Carousel.Item>
+          <Carousel.Item interval={500}>
+            <img
+              className="d-block w-100 mx-auto"
+              src="https://t4.ftcdn.net/jpg/02/05/25/95/240_F_205259502_5xqjcHxIiuGFekk0BMyVy9G9Oaz8Ars6.jpg"
+              alt="Second slide"
+            />
+            
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 mx-auto"
+              src="https://t4.ftcdn.net/jpg/02/16/66/29/240_F_216662905_oZJORSfPuVWSFKagVhI7qnR1612uzogd.jpg"
+              alt="Third slide"
+            />
+          
+          </Carousel.Item>
+        </Carousel>
+            </div>
+    );
+};
+
+export default Banner;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Banner from '../Component/Banner/Banner';
 import Header from '../Component/Header/Header';
+import WeDo from '../Component/We Do/WeDo';
 
 const Home = () => {
     return (
         <div class='container'>
            <Header></Header> 
+           <Banner></Banner>
+           <WeDo></WeDo>
         </div>
     );
 };
